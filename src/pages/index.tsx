aefdbed24d3abe9dfb7356f41b23a094657d023b
Amarse <byicy08@gmail.com>
2022-12-13 22:11:33 +0900

    init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
-import Head from 'next/head'
-import Image from 'next/image'
+// import { useRouter } from 'next/router';
+// import { useEffect } from 'react';
 
 export default function Home() {
+  // const router = useRouter();
+
+  // useEffect(() => {
+  //   router.push('/store', undefined, { shallow: true });
+  // }, []);
+
+  // useEffect(() => {
+  // }, [router.query.counter]);
+
   return (
     <div>
    
     </div>
-  )
+  );
 }
